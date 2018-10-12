33212ab807e1de83380f137224dc5c480c6b1bd9
Martin <Martin@MyM>
2018-10-12 00:54:56 -0300

    10/12/2018 - created the cityOfAaron project for class.

---
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -0,0 +1,22 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * cIT-260
+ * Fall 2018
+ * Team members: Jake Davis, Martin Cabrera
+ */
+package cityofaaron;
+
+/**
+ *
+ * @author Martin
+ */
+public class CityOfAaron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
