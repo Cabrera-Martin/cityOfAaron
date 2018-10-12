fd9c570214835b296abb5dc24f19a33796aad62d
Martin <Martin@MyM>
2018-10-12 02:58:31 -0300

    10/12/2018 - created the cityOfAaron project for class.

---
+++ cityOfAaron/src/model/TeamMembers.java
@@ -0,0 +1,35 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ * Team members: Jake Davis, Martin Cabrera
+ */
+package model;
+
+/**
+ *
+ * @author Martin
+ */
+public enum TeamMembers
+{
+    
+   MC("Tincho","Mister"),
+   Dasammy("Dasammy","Blue");
+   
+private String name;
+private String title;
+        
+TeamMembers(String_name, String_title)
+{
+name=_name;
+title=_title;
+}
+public String getName()
+{
+return name;
+}
+public String getTitle()
+{
+return title;
+}
+}
