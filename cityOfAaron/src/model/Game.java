9de7e1fd5ce4c39f9bd220f9a1cdb02897ef7306
Martin <Martin@MyM>
2018-10-13 09:50:57 -0300

    10/12/2018 - Game and Player class

---
+++ cityOfAaron/src/model/Game.java
@@ -0,0 +1,24 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ * Team members: Jake Davis, Martin Cabrera
+ */
+package model;
+
+/**
+ *
+ * @author Martin
+ */
+import java.io.Serializable;
+public class Game implements Serializable{
+    private Player thePlayer;
+    
+    public Game(){}
+    public String setPlayer()
+    {
+     return thePlayer;
+    }
+        public void getPlayer(player_thePlayer){
+        thePlayer=_thePlayer;
+        }
---
+++ cityOfAaron/src/model/Player.java
@@ -0,0 +1,28 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ * Team members: Jake Davis, Martin Cabrera
+ */
+package model;
+
+/**
+ *
+ * @author Martin
+ */
+import java.io.Serializable;
+public class Player implements Serializable{
+    
+    private String name;
+    
+    public Player(){}
+    public String getName()
+    {
+     return name;
+    }
+        public void setName(string_name){
+    name=_name;
+    
+    }
+    
+}
