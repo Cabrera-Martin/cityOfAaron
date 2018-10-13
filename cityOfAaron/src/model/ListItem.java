2e93f7287b0c7e938a18c090c34706b35f6ab326
Jake Davis <>
2018-10-13 15:51:28 -0700

    10/13/2018 Program for week 4 CIT 260

---
+++ cityOfAaron/src/model/ListItem.java
@@ -0,0 +1,34 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ * Team members: Jake Davis, Martin Cabrera
+ */
+package model;
+
+/**
+ *
+ * @author Jake Davis
+ */
+import java.io.Serializable;
+public class ListItem implements Serializable{ 
+ 
+    private String name;
+    private int number;
+    
+    public ListItem(){
+    }
+    public String getName()
+    {
+     return name;
+    }
+        public void setName(String _name){
+    name= _name;
+    }
+    public int getNumber(){
+        return number;
+    }
+    public void setNumber(int _number) {
+        number= _number;
+    }
+}
