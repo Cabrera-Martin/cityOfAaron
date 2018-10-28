5d2cb042827217b546da14d132b569364f6847a7
Martin <Martin@MyM>
2018-10-28 00:08:40 -0300

    10/27/2018 - Individual assignments

--- cityOfAaron/src/control/CropControl.java
+++ cityOfAaron/src/control/CropControl.java
@@ -8,18 +8,6 @@ package control;
 import java.util.Random;
 import model.CropData;
 
-/**
-* The buyLand method
-* Purpose: To buy land
-* @param the price of land
-* @param the number of acres to buy
-* @param a reference to a CropData object
-* @ return the number of total acres after the purchase
-* Pre-conditions: acres to buy must be positive
-* and <= than the bushel of wheat owned * landPrice
-*/
-
-
 public class CropControl 
 {
      // constants
@@ -38,6 +26,14 @@ public class CropControl
         return landCost;            
     }
 
+//The buyLand method
+//Purpose: To buy land
+//@param the price of land
+//@param the number of acres to buy
+//@param a reference to a CropData object
+//@ return the number of total acres after the purchase
+//Pre-conditions: acres to buy must be positive
+//and <= than the bushel of wheat owned * landPrice
     public static int buyLand(int landCost, int acresToBuy, CropData cropData)
     {
         //if acresToBuy < 0, return -1
@@ -58,5 +54,61 @@ public class CropControl
         //return acresOwned
         return owned;
     }
-}
+// The plantCrop method
+//Purpose: To plant crops land
+//@param the amount of people in the city
+//@param the number of acres to plant
+//@param the number of acres owned
+//@param the number of bushels of wheat stored
+//@param a reference to a CropData object
+//@ return the number of total acres planted
+//@ return the number of total bushels of wheat left in store
+//Pre-conditions: acres to plant must be positive, acres to plant is <= acres owned, acres to be //planted / 10 <= amount of people in the city abd the acres to plant / 2 <= than bushel of
+//wheat in storage
+  
+    public static int plantCrops(int acresToPlant, CropData cropData)
+    {
+    //if acresToPlant < 0, return -1
+        if(acresToPlant < 0)
+            return -1;
+        //if acresToPlant > acresOwned, return -1        
+        int owned = cropData.getacresOwned();
+        if(acresToPlant > owned)
+             return -1;
+        //if acresToPlant/2 > bushelInStorage, return -1
+        int bushel = cropData.getwheatInStore();
+        if(acresToPlant/2 > bushel)
+             return -1;
+        
+        //if acresToPlant/10 > population, return -1
+        int pop = cropData.getpopulation();
+        if(acresToPlant/10 > pop)
+             return -1;
+        //acresPlanted = acresPlanted + acresToPlant
+        int planted = cropData.getacresPlanted();
+        planted+= (acresToPlant);
+        cropData.setacresPlanted(planted);
+        
+        //wheatInStore = wheatInStore - (acresToPlant/2)
+        int wheat = cropData.getwheatInStore();
+        wheat-= (acresToPlant/2);
+        cropData.setwheatInStore(wheat);
+        //return acresPlanted and wheatInStore 
+      System.out.println("Acres Planted = " + planted + "What in Store = " + wheat);
+         return wheat;
+    }
+//The seOffering method
+// Purpose: To set the percentage of offering the player wants to allocate
+// @param percentage to pay
+//@ return the percentage to pay or an error
 
+    public static int setOffering(int percentageToPay)
+    {
+//if percentageToPay < 0 and > 1, return -1
+        if(percentageToPay < 0 && percentageToPay > 1)
+            return -1;
+//if percentageToPay > 0 and < 1, return the percentageToPay
+        if(percentageToPay > 0 && percentageToPay < 1)
+            return percentageToPay;
+    }
+}
