c933faf3de344f549e3f938b20f8730ed8e33b1c
Martin <Martin@MyM>
2018-11-22 00:35:15 -0300

    Display the CropReview when selecting option Manage Crops on the Game Menu - Nov 14th 2018 - Martin

--- cityOfAaron/src/control/GameControl.java
+++ cityOfAaron/src/control/GameControl.java
@@ -9,18 +9,26 @@ import model.*;
 import cityofaaron.CityOfAaron;
 
 public class GameControl {
+    
+    // size of the Locations array
+    private static final int MAX_ROW = 5;
+    private static final int MAX_COL = 5;
+    private static final Game game = new Game();
+    
     public static void createNewGame(String _name)
-    {
-        
-        // create the game object
-        Game game = new Game();
-        
+    {              
         // create the player object and set the name
         Player player = new Player();
         player.setName(_name);
         
         // save reference to the player object in the game
         game.setPlayer(player);
+      
+        // when all is done, save a reference to the Game object
+        CityOfAaron.setGame(game);  
+    }
+    public static void createCropDataObject()
+       {        
         // create and initialize a CropData object
         // save a reference to it in the Game object
         CropData cropData = new CropData();
@@ -35,10 +43,42 @@ public class GameControl {
         cropData.setacresPlanted(1000);
         cropData.setharvest(3000);
         cropData.setacresPlanted(1000); 
-        game.setcropData(cropData); 
-        
-        // when all is done, save a reference to the Game object
-        CityOfAaron.setGame(game);   
-        
-    }
+        game.setcropData(cropData);       
+       }      
+        // The createMap method
+        // Purpose: creates the location objects and the map
+        // Parameters: none
+        // Returns: none
+    public static void createMap()   
+        {
+        // create the Map object, it is 5 x 5
+        // refer to the Map constructor
+        Map theMap = new Map(MAX_ROW, MAX_COL);
+        // create a string that will go in the Location objects
+        // that contain the river
+        String river = "\nYou are on the River. The river is the source" +
+                       "\nof life for our city. The river marks the eastern " +
+                       "\nboundary of the city - it is wilderness to the East.";
+        // create a new Location object
+        Location loc = new Location();
+        // use setters in the Location class to set the description and symbol
+        loc.setDescription(river);
+        loc.setSymbol("~~~");  
+        // set this location object in each cell of the array in column 4      
+        for(int i = 0; i < MAX_ROW; i++)
+            {
+            theMap.setLocation(i, 4, loc);
+            }
+            // define the string for a farm land location
+            String farmland = "\nYou are on the fertile banks of the River." +
+                              "\nIn the springthis low farmland floods and is covered with rich" +
+                              "\nnew soil. Wheat is planted as far as you can see."; 
+
+            // set a farmland location with a hint
+            loc = new Location();
+            loc.setDescription(farmland + "\nOne bushel will plant two acres of wheat.");
+            loc.setSymbol("!!!");
+            theMap.setLocation(0, 2, loc);
+
+        }
 }
